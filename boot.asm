bd8922aa4cbd2154fe9b608cfa94847a97e969ec
Mejd Almohamadi <mejd.almohamadi765@gmail.com>
2021-12-23 21:36:24 +0300

    update

--- boot.asm
+++ boot.asm
@@ -1,3 +1,4 @@
+int 0x13
 txtprint:
     db "Operating system is being executed", 0x00
 
@@ -7,41 +8,18 @@ err:
 finshSuccess:
     db "Operating system successful", 0x00
 
-mov dx, 100
-mov cx, 0
-
-pitx:
-    mov al, 0x36
-    out 0x43, al    ;tell the PIT which channel we're setting
-
-    mov ax, 11931
-    out 0x40, al    ;send low byte
-    mov al, ah
-    out 0x40, al    ;send high byte
-    inc cx
-    cmp cx, dx
-    jle pitx
-
 print:
     mov ah, 0x0e
-    mov al, bl
+    mov al, [bx]
     int 0x10
-    inc al
+    inc bx
     cmp al, 0x00
     jne print
 
-errorPrt:
-    mov bl, [err + 0x7c00]
-    jmp print
-
-success:
-    mov bl, [finshSuccess + 0x7c00]
-    jmp print
-
-mov bl, [txtprint + 0x7c00]
+mov bx, [txtprint + 0x7c00]
 jmp print
-jmp pitx
-jmp success
+mov bx, [finshSuccess + 0x7c00]
+jmp print
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
