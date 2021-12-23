934d3d42f7b3fbec363f79c952c6caa27a7b0f30
Mejd Almohamadi <mejd.almohamadi765@gmail.com>
2021-12-23 21:57:39 +0300

    Update

--- boot.asm
+++ boot.asm
@@ -1,12 +1,10 @@
-int 0x13
 txtprint:
-    db "Operating system is being executed", 0x00
-
-err:
-    db "INTERRUPT FAILLED, PLS FIX", 0x00
+    db "Operating system is being executed", 0
 
 finshSuccess:
-    db "Operating system successful", 0x00
+    db "Operating system successful", 0
+
+mov bx, [txtprint + 0x7c00]
 
 print:
     mov ah, 0x0e
@@ -14,12 +12,12 @@ print:
     int 0x10
     inc bx
     cmp al, 0x00
-    jne print
+    je end
+    jmp print
+end:
 
-mov bx, [txtprint + 0x7c00]
-jmp print
-mov bx, [finshSuccess + 0x7c00]
-jmp print
+
+jmp $
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
